61358f21ab35a09ed1a8f716d0e0c7a94076db0d
Kiko <mmhabib11@gmail.com>
2015-05-13 04:00:58 -0400

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+	skip_before_filter :require_login, only: [:index, :new, :create]
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to(:users, notice: 'User was successfully created')
+		else
+			render 'new', :notice => "Error signing up"
+		end
+	end
+
+	def show
+		@user = User.find(params(:id))
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :admin)
+	end
+end
