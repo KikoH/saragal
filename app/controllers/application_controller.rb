55604722eb9080be618d7207719827656fde6167
Kiko <mmhabib11@gmail.com>
2015-05-14 19:36:07 -0400

    Restriction to edit only when current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+   def findUser
+ 		@user = User.find(params[:id])
+  end
+
   private
   def not_authenticated
   	redirect_to login_path, alert: "Please login first"
