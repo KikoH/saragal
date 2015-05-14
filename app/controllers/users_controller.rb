a402211847e2fc1b669c6119e3f580a982053bf5
Kiko <mmhabib11@gmail.com>
2015-05-14 19:37:42 -0400

    User update, edit and delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,22 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to user_path(@user)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to paintings_path
 	end
 
 	private
