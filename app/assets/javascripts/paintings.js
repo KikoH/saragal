71ce92a87b0bb6d4ed9a1b16a7d8d024af5f4cb2
Kiko <mmhabib11@gmail.com>
2015-06-05 16:37:35 -0400

    More turn styling

--- app/assets/javascripts/paintings.js
+++ app/assets/javascripts/paintings.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+var ready = function() {
 	$('#book').turn({
 		display: 'double',
 		autoCenter: true
 	});
+};
 
-});
+$(document).ready(ready);
+$(document).on('page:load', ready);
 
 $(window).bind('keydown', function(e) {
 	if (e.keyCode== 37)
